e5241f8b89938557f24313581212fb22a72cfd6b
Gabriel N. Camargo-Toledo <gcamargo@sensata.io>
2021-10-14 12:21:54 -0500

    keeping only graphData functions for alimentación

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Sensata data example from Alimentación project
+#'
+#' A dataset of survey 2831 surveys selected from Colombia Alimentación Saludable Data
+#' sensataExAnalysis
+#'
+#' @format A data frame with 2831 rows and 86 variables:
+#' \describe{
+#'   \item{Group}{experimental group}
+#'   \item{DV}{experimenta dependent variable}
+#' }
+#' @source Alimentación saludable filter for Colombia and only people with the experiment
+"sensataExAnalysis"
---
+++ R/sensataDataAnalysis-package.R
@@ -0,0 +1,20 @@
+#' sensataDataAnalysis: Analyzing sensata survey data
+#'
+#' @description
+#' This package has the functions most commonly used to analyze survey data from Sensata platform
+#'
+#' Este paquete tiene las funciones usadas comunmente para analizar los datos de encuestas de la plataforma de Sensata
+#'
+#' @section sensataDataAnalysis basic functions:
+#' LLENAR ESTA LISTA CON FUNCIONES PRINCIPALES
+#'
+#' @section Additional functions:
+#' LLENAR ESTA SECCIÓN CON OTROS PAQUETES
+#'
+#' @source \url{https://github.com/GaborioSensata/sensataDataAnalysis}
+#'
+#'
+#' @docType package
+#' @name sensataDataAnalysis
+NULL
+#> NULL
