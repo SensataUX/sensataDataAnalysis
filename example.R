42641f4318a1101545fd012391a4c38fe1051425
Gabriel N. Camargo-Toledo <gcamargo@sensata.io>
2021-11-24 15:04:59 -0500

    example

--- example.R
+++ example.R
@@ -4,7 +4,7 @@ load_all()
 sensataExAnalysis <- sensataExAnalysis
 
 # 5 options (likert)
-createGraphData(df = sensataExAnalysis, originVar = "q_EA_IN_03", groupVar = "q_EA_CA_10")
+createGraphData(df = sensataExAnalysis, originVar = "q_EA_IN_03",groupVar = "q_EA_CA_10")
 
 # 4 options (text)
 createGraphData(df = sensataExAnalysis, originVar = "q_EA_IN_03", groupVar = "q_EA_CA_10")
